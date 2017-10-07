298a1749e594fdb5e1fd5873dc457664568a49f0
tdimnet <thomas.dimnet@gmail.com>
2017-10-07 19:16:55 +0200

    Make the api calls work by setting up endpoints for get, post and get a specific id request

--- routes.js
+++ routes.js
@@ -6,6 +6,31 @@ const express = require('express');
 // Setting up the express router
 const router = express.Router();
 
+// GET /questions
+  // Route for returning questions collection
+router.get('/', function (req, res) {
+  res.json({
+    response: "You sent me a GET request"
+  });
+});
+
+// POST /questions
+  // Route for creating questions
+router.post('/', function (req, res) {
+  res.json({
+    response: "You sent me a POST request",
+    body: req.body
+  });
+});
+
+// GET /questions/:id
+  // Route for returning a specific question
+router.get('/:id', function (req, res) {
+  res.json({
+    response: "You sent me a GET request for a specific ID " + req.params.id,
+
+  });
+});
 
 // Exporting the router
 module.exports = router;
