19d187c91b2693e6181ccf92596ff46483037b42
tdimnet <thomas.dimnet@gmail.com>
2017-10-07 18:26:03 +0200

    Start working with the API and middlewares

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const express = require('express');
 // Set up the express app
 const app = express();
 
+
+app.use(function(req, res, next) {
+  console.log('The leaves on the trees are ', req.query.color);
+  next();
+});
+
+
 // Set up the port
 const port = process.env.PORT || 3000;
 
